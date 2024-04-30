0baf26b866231939f51f22f6955e49c22881bd2e
Terrell <1627565054@qq.com>
2024-04-30 10:42:07 +0800

    生效.gitignore

--- src/main/java/com/ggzn/jenkins/Main.java
+++ src/main/java/com/ggzn/jenkins/Main.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
         SpringApplication.run(Main.class);
     }
